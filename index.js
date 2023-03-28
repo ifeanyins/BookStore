41281b9d8221ef3207b78601a92a6023c6f1c6cf
ifeanyi_ns <ifeanyi_ns@icloud.com>
2023-03-28 02:16:56 +0100

    building of the view Section

--- index.js
+++ index.js
@@ -1,73 +1,111 @@
-const books = [
-    {
-        Title: "48 LAWS OF POWER",
-        Author: "Robert Greene"
-    },
-    {
-        Title: "THINK AND GROW RICH",
-        Author: "Napoleon Hill"
-    },
-    {
-        Title: "THINK AND GROW RICH",
-        Author: "Napoleon Hill"
-    },
-    {
-        Title: "THINK AND GROW RICH",
-        Author: "Napoleon Hill"
+
+
+class Book {
+    constructor(title, author){
+        this.title = title;
+        this.author = author;
     }
-]
+}
 
-const titleHeader = document.getElementById('uk-header-text')
-titleHeader.textContent = "Awesome books"
-const inputDiv =document.getElementById ("uk-input-div");
+// call the books and add them to the local storage
+class SavedBooks {
+    static getBook(){
+    let todoArr;
+        if (localStorage.getItem('todoArr') === null) {
+            todoArr = [];
+        } else {
+            todoArr = JSON.parse(localStorage.getItem('todoArr'));
+        }
+        return todoArr;
+    }
 
-function breaking (){
-    
-    books.map(book => {
-        const mainDiv = document.querySelector('#uk-emptydiv')
-       const childDiv = document.createElement('div');
-       const title = document.createElement('h3');
-       const author = document.createElement('h5');
-       const removeBtn = document.createElement('button');
-       removeBtn.innerHTML = "Remove";
-       removeBtn.classList.add("removebtn")
-       const hrLine = document.createElement('hr');
-        title.textContent = `${book.Title}`;
-        author.textContent = `${book.Author}`;
-        childDiv.appendChild(title);
-        childDiv.appendChild(author);
-        childDiv.appendChild(removeBtn);
-        childDiv.appendChild(hrLine);
-        mainDiv.appendChild(childDiv);
+    static addBook(book) {
+       const todoArr = SavedBooks.getBook();
+       todoArr.push(book);
+       localStorage.setItem('books', JSON.stringify(books))
+    }
+
+    // counts the number of books present from the local storage
+  static bookNumber(){
+    const todoArr = JSON.parse(ocalStorage.getItem('todoArr')) || [];
+    return todoArr.length;
+  }
+}
+
+// the user class that displays the book at the top
+let books;
+class User {
+    static showBooks(){
+        todoArr = SavedBooks.getBook();
+        todoArr.forEach(book => {
+            User.showBookList(book)
+        });
+    }
+
+    static showBookList (book){
+        // Creation of element / DOM Manipulation
+    const mainDiv = document.getElementById('uk-emptydiv');
+    const bookDiv = document.createElement('div')
+    bookDiv.className = 'book-container';
+    const titleDiv = document.createElement('h5')
+    const authorDiv = document.createElement('h5')
+    const removeBtn = document.createElement('button')
+
+    titleDiv.innerText = `${book.title}`
+    authorDiv.innerText = `${book.author}`
+    const bookReferences = document.createElement('p')
+    bookReferences.innerText = `${book.title} by ${book.author}`
+    removeBtn.innerText = 'Remove';
+    removeBtn.className = 'btn';
+    bookDiv.appendChild(bookReferences)
+    bookDiv.appendChild(removeBtn)
+    mainDiv.appendChild(bookDiv)
+    // Add event listener
+    removeBtn.addEventListener('click', ()=> {
+        User.deleteBook(removeBtn);
+    })
+}
+    static clearInput() {
+        document.getElementById('uk-author').value = '';
+        document.getElementById('uk-title').value = '';
+    }
+
+    // remove Button Function
+    static deleteBook(removeBtn) {
+        const todoArr = SavedBooks.getBook();
+        const bookTitle = removeBtn.parentElement.firstElementChild.innerText;
+        const filteredBooks = todoArr.filter((book) => book.title !== bookTitle.slice(0, bookTitle.indexOf('by') - 1));
+        localStorage.setItem('todoArr', JSON.stringify(filteredBooks));
+        removeBtn.parentElement.remove();
+    }
+
+}
+
+document.querySelector('#addbtn').addEventListener('click', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('uk-title').value;
+    const author = document.getElementById('uk-author').value;
+    const bookAddedMsg = document.getElementById('display-msg');
+    const message = document.createElement('p');
+    message.className = 'message';
+    bookAddedMsg.appendChild(message);
+
+    if (!title || !author) {
+        message.innerText = 'Title and author cannot be empty';
+        message.classList = 'error';
+      } else {
+        const book = new Book(title, author);
+        SavedBooks.addBook(book);
+        User.showBookList(book);
+        User.clearInput();
+        message.innerText = 'Great! Your book was successfully added!!';
+      }
+      setTimeout(() => {
+        message.textContent = '';
+        message.style.display = 'none';
+      }, 3000);
     });
 
-}
-
-breaking()
-
-function inputs (){
-    const titleInput = document.createElement('input')
-    titleInput.classList.add("titleC");
-    titleInput.placeholder = "Title";
-    const authorInput = document.createElement('input');
-    authorInput.classList.add("titleC");
-    authorInput.placeholder = "Author";
-    const lineBr = document.createElement('br');
-    const lineB = document.createElement('br');
-    const addBtn = document.createElement('button');
-    addBtn.classList.add("addbtn")
-    const mainInputDiv = document.createElement('div')
-    
-    
-    mainInputDiv.appendChild(titleInput);
-    mainInputDiv.appendChild(lineBr);
-    mainInputDiv.appendChild(lineB);
-    mainInputDiv.appendChild(authorInput);
-    mainInputDiv.appendChild(addBtn);
-    inputDiv.appendChild(mainInputDiv)
-
-    addBtn.innerHTML = "Add"
-}
-
-inputs ()
-
+// const headerText = document.getElementById('uk-header-text');
+// const titleInput = document.getElementById('uk-title');
+// const authorInput = document.querySelector('#uk-author');

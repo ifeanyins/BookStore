97df52d3ee8cad622fbca6c5bc01f51c30d2515a
ifeanyi_ns <ifeanyi_ns@icloud.com>
2023-03-30 15:14:43 +0100

    linking the List to the local storage

--- index.js
+++ index.js
@@ -26,20 +26,6 @@ function showPanel(panelId){
     document.getElementById(panelId).classList.remove('hide')
 }
 
-// function showPanel (panelIndex, colorCOde) {
-//     tabButton.forEach(function(node){
-//         node.style.backgroundColor= "";
-//         node.style.color= "";
-//     });
-//     tabButton[panelIndex].style.backgroundColor = colorCOde;
-//     tabButton[panelIndex].style.color = 'white';
-//         tabPanel.forEach(function(node){
-//             node.style.display = "none";
-//         })
-//     tabButton[panelIndex].style.display = 'block';
-// };
-// showPanel(0)
-
 
 class Book {
     constructor(title, author){
@@ -49,63 +35,63 @@ class Book {
 }
 
 // call the books and add them to the local storage
-class SavedBooks {
-    static getBook(){
-    let todoArr;
-        if (localStorage.getItem('todoArr') === null) {
-            todoArr = [];
-        } else {
-            todoArr = JSON.parse(localStorage.getItem('todoArr'));
-        }
-        return todoArr;
-    }
-
-    static addBook(book) {
-       const todoArr = SavedBooks.getBook();
-       todoArr.push(book);
-       localStorage.setItem('books', JSON.stringify(books))
-    }
-
-    // counts the number of books present from the local storage
-  static bookNumber(){
-    const todoArr = JSON.parse(ocalStorage.getItem('todoArr')) || [];
-    return todoArr.length;
+class   SavedBooks {
+  static getBook(){
+  let books;
+      if (localStorage.getItem('books') === null) {
+          books = [];
+      } else {
+          books = JSON.parse(localStorage.getItem('books'));
+      }
+      return books;
   }
+
+  static addBook(book) {
+     const books = SavedBooks.getBook();
+     books.push(book);
+     localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  // counts the number of books present from the local storage
+static bookNumber(){
+  const books = JSON.parse(localStorage.getItem('books')) || [];
+  return books.length;
+}
 }
 
 // the user class that displays the book at the top
 let books;
 class User {
-    static showBooks(){
-        todoArr = SavedBooks.getBook();
-        todoArr.forEach(book => {
-            User.showBookList(book)
-        });
-    }
+  static showBooks(){
+      books = SavedBooks.getBook();
+      books.forEach(book => {
+          User.showBookList(book)
+      });
+  }
+  static showBookList (book){
+    // Creation of element / DOM Manipulation
+  const mainDiv = document.getElementById('uk-emptydiv');
+  let list = document.querySelector('#showList')
+  const bookDiv = document.createElement('div')
+  bookDiv.className = 'book-container';
+  const titleDiv = document.createElement('h5')
+  const authorDiv = document.createElement('h5')
+  const removeBtn = document.createElement('button')
 
-    static showBookList (book){
-        // Creation of element / DOM Manipulation
-    const mainDiv = document.getElementById('uk-emptydiv');
-    const bookDiv = document.createElement('div')
-    bookDiv.className = 'book-container';
-    const titleDiv = document.createElement('h5')
-    const authorDiv = document.createElement('h5')
-    const removeBtn = document.createElement('button')
 
-
-    titleDiv.innerText = `${book.title}`
-    authorDiv.innerText = `${book.author}`
-    const bookReferences = document.createElement('p')
-    bookReferences.innerText = `${book.title} by ${book.author}`
-    removeBtn.innerText = 'Remove';
-    removeBtn.className = 'btn';
-    bookDiv.appendChild(bookReferences)
-    bookDiv.appendChild(removeBtn)
-    mainDiv.appendChild(bookDiv)
-    // Add event listener
-    removeBtn.addEventListener('click', ()=> {
-        User.deleteBook(removeBtn);
-    })
+  titleDiv.innerText = `${book.title}`
+  authorDiv.innerText = `${book.author}`
+  const bookReferences = document.createElement('p')
+  bookReferences.innerText = `${book.title} by ${book.author}`
+  removeBtn.innerText = 'Remove';
+  removeBtn.className = 'btn';
+  bookDiv.appendChild(bookReferences)
+  bookDiv.appendChild(removeBtn)
+  list.appendChild(bookDiv)
+  // Add event listener
+  removeBtn.addEventListener('click', ()=> {
+      User.deleteBook(removeBtn);
+})
 }
     static clearInput() {
         document.getElementById('uk-author').value = '';
@@ -114,11 +100,11 @@ class User {
 
     // remove Button Function
     static deleteBook(removeBtn) {
-        const todoArr = SavedBooks.getBook();
-        const bookTitle = removeBtn.parentElement.firstElementChild.innerText;
-        const filteredBooks = todoArr.filter((book) => book.title !== bookTitle.slice(0, bookTitle.indexOf('by') - 1));
-        localStorage.setItem('todoArr', JSON.stringify(filteredBooks));
-        removeBtn.parentElement.remove();
+      const books = SavedBooks.getBook();
+      const bookTitle = removeBtn.parentElement.firstElementChild.innerText;
+      const filteredBooks = books.filter((book) => book.title !== bookTitle.slice(0, bookTitle.indexOf('by') - 1));
+      localStorage.setItem('books', JSON.stringify(filteredBooks));
+      removeBtn.parentElement.remove();
     }
 
 }
@@ -148,5 +134,7 @@ document.querySelector('#addbtn').addEventListener('click', (e) => {
       }, 3000);
     });
 
+    const listNum = document.getElementById('bookCounter')
+    listNum.innerText = `${SavedBooks.bookNumber()}`
 // const titleInput = document.getElementById('uk-title');
 // const authorInput = document.querySelector('#uk-author');

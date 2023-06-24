962e865dc1cc7ac661050c74424c803dedeffb9e
ifeanyi_ns <ifeanyi_ns@icloud.com>
2023-06-24 11:35:42 +0100

    addition of the navbar toggle

--- index.js
+++ index.js
@@ -17,124 +17,17 @@ currentDate.classList.add('current')
 // Tab content switch
 
 let tabPanel = document.querySelectorAll('.tabPanel');
-
-function showPanel(panelId){
-    tabPanel.forEach((panel) =>
-       panel.classList.add('hide')
-    );
-
+function showPanel(panelId) {
+tabPanel.forEach(element => {
+    element.classList.add('hide');
+});
     document.getElementById(panelId).classList.remove('hide')
 }
 
+// const changeNavColor = (index) => {
+//     const navItems = document.querySelectorAll('#nav-item');
+//     Array.from(navItems).forEach((item, ind) => (ind === index ? item.classList.toggle('clicked-link') : item.classList.remove('clicked-link')));
+//   };
+//   window.changeNavColor = changeNavColor;
+  
 
-class Book {
-    constructor(title, author){
-        this.title = title;
-        this.author = author;
-    }
-}
-
-// call the books and add them to the local storage
-class   SavedBooks {
-  static getBook(){
-  let books;
-      if (localStorage.getItem('books') === null) {
-          books = [];
-      } else {
-          books = JSON.parse(localStorage.getItem('books'));
-      }
-      return books;
-  }
-
-  static addBook(book) {
-     const books = SavedBooks.getBook();
-     books.push(book);
-     localStorage.setItem('books', JSON.stringify(books))
-  }
-
-  // counts the number of books present from the local storage
-static bookNumber(){
-  const books = JSON.parse(localStorage.getItem('books')) || [];
-  return books.length;
-}
-}
-
-// the user class that displays the book at the top
-let books;
-class User {
-  static showBooks(){
-      books = SavedBooks.getBook();
-      books.forEach(book => {
-          User.showBookList(book)
-      });
-  }
-  static showBookList (book){
-    // Creation of element / DOM Manipulation
-  const mainDiv = document.getElementById('uk-emptydiv');
-  let list = document.querySelector('#showList')
-  const bookDiv = document.createElement('div')
-  bookDiv.className = 'book-container';
-  const titleDiv = document.createElement('h5')
-  const authorDiv = document.createElement('h5')
-  const removeBtn = document.createElement('button')
-
-
-  titleDiv.innerText = `${book.title}`
-  authorDiv.innerText = `${book.author}`
-  const bookReferences = document.createElement('p')
-  bookReferences.innerText = `${book.title} by ${book.author}`
-  removeBtn.innerText = 'Remove';
-  removeBtn.className = 'btn';
-  bookDiv.appendChild(bookReferences)
-  bookDiv.appendChild(removeBtn)
-  list.appendChild(bookDiv)
-  // Add event listener
-  removeBtn.addEventListener('click', ()=> {
-      User.deleteBook(removeBtn);
-})
-}
-    static clearInput() {
-        document.getElementById('uk-author').value = '';
-        document.getElementById('uk-title').value = '';
-    }
-
-    // remove Button Function
-    static deleteBook(removeBtn) {
-      const books = SavedBooks.getBook();
-      const bookTitle = removeBtn.parentElement.firstElementChild.innerText;
-      const filteredBooks = books.filter((book) => book.title !== bookTitle.slice(0, bookTitle.indexOf('by') - 1));
-      localStorage.setItem('books', JSON.stringify(filteredBooks));
-      removeBtn.parentElement.remove();
-    }
-
-}
-
-document.querySelector('#addbtn').addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = document.getElementById('uk-title').value;
-    const author = document.getElementById('uk-author').value;
-    const bookAddedMsg = document.getElementById('display-msg');
-    const message = document.createElement('p');
-    message.className = 'message';
-    bookAddedMsg.appendChild(message);
-
-    if (!title || !author) {
-        message.innerText = 'Title and author cannot be empty';
-        message.classList = 'error';
-      } else {
-        const book = new Book(title, author);
-        SavedBooks.addBook(book);
-        User.showBookList(book);
-        User.clearInput();
-        message.innerText = 'Great! Your book was successfully added!!';
-      }
-      setTimeout(() => {
-        message.textContent = '';
-        message.style.display = 'none';
-      }, 3000);
-    });
-
-    const listNum = document.getElementById('bookCounter')
-    listNum.innerText = `${SavedBooks.bookNumber()}`
-// const titleInput = document.getElementById('uk-title');
-// const authorInput = document.querySelector('#uk-author');
